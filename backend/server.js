089b23960dcd799af1d4ac142ac8048b4af0567a
nikhilkumar50 <nikhilkumarray9889@gmail.com>
2024-02-21 19:53:56 +0530

    update certain section

--- backend/server.js
+++ backend/server.js
@@ -20,13 +20,11 @@ const PORT = process.env.PORT || 8000;
 const __dirname = path.resolve();
 
 app.use(session({ secret: "keyboard cat", resave: false, saveUninitialized: false }));
-// Initialize Passport!  Also use passport.session() middleware, to support
-// persistent login sessions (recommended).
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Here we can remove the cors, it's not necessary in production because the frontend and backend are on the same domain. I forgot to mention that in the video, sorry about that.🙄
-// app.use(cors());
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
